a88add211254ffb96771a271ed359b32963f804a
FrancoCaneti <CAnetifranco95@hmail.com>
2021-03-19 15:40:11 +0100

    creazione file

---
+++ script.js
@@ -0,0 +1,13 @@
+//Quanti km devi percorrere?
+var km = prompt ('Quanti km vuoi percorrere?');
+console.log('Quanti km deve percorrere?',km)
+
+
+//Età
+var età = prompt ('Quanti anni hai?');
+console.log('quanti anni ha',età);
+
+//Euro
+var prezzo = km  * 0.21 
+ console.log('prezzo da pagare:',prezzo)
+
