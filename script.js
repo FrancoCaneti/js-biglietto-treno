956055452c9f7ec6a40278009f33324b38b7b2f2
FrancoCaneti <CAnetifranco95@hmail.com>
2021-03-21 17:03:01 +0100

    if else

--- script.js
+++ script.js
@@ -4,10 +4,33 @@ console.log('Quanti km deve percorrere?',km)
 
 
 //Età
-var età = prompt ('Quanti anni hai?');
-console.log('quanti anni ha',età);
+var age = prompt ('Quanti anni hai?');
+console.log('quanti anni ha',age);
 
-//Euro
+//Prezzo
 var prezzo = km  * 0.21 
- console.log('prezzo da pagare:',prezzo)
+ 
+ 
+//Prezzo Under 18
+var scontoUnder = prezzo * 20 / 100;
+var prezzoUnder = prezzo - scontoUnder;
 
+ //Prezzo Over 65  
+ var scontoOver = prezzo * 40 / 100;
+ var prezzoOver = prezzo - scontoOver; 
+ console.log('prezzo over:',prezzoOver);
+ 
+
+//Prezzo finale
+var prezzoFinale
+if ( age < 18 ) {
+    prezzoFinale = prezzoUnder + '$';
+} else if ( age >=  18 ) {
+    prezzoFinale = prezzo +'$';
+} else if ( age > 65) {
+    prezzoFinale = prezzoUnder + '$';
+}
+
+    
+  console.log('Il prezzo da pagare è:'+ prezzoFinale )       
+    
